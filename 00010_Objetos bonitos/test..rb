7f1b310bd14ae3c43911271d3b70397f42f86f80
franco@mumuki.org <franco@mumuki.org>
2016-11-24 11:11:18 -0500

    Mumuki Export on 2016-11-24 11:11:18 -0500

--- 00010_Objetos bonitos/test..rb
+++ 00010_Objetos bonitos/test..rb
@@ -9,6 +9,6 @@ end
 describe 'Pretty' do 
   it { expect(Pretty.new.prettify).to eq "latitud: 0\nlongitud: 0" }
   it { expect(Pretty.new(20, 30).prettify).to eq "latitud: 20\nlongitud: 30" }
-  it { expect(Sample.new.pretty).to eq "foo: 3\n bar: 5\n goo: 6" 
+  it { expect(Sample.new.pretty).to eq "foo: 3\n bar: 5\n goo: 6" }
 end
 
